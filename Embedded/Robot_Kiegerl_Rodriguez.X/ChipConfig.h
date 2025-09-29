3a0b2d138782a20ec597607fc34bcff4d5a3a319
iutgeiitoulonE105 <70626387+iutgeiitoulonE105@users.noreply.github.com>
2025-09-29 15:47:24 +0200

    led operationnelles

---
+++ Embedded/Robot_Kiegerl_Rodriguez.X/ChipConfig.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   ChipConfig.h
+ * Author: E306-PC4
+ *
+ * Created on September 29, 2025, 2:39 PM
+ */
+
+/* 
+ * File:   ChipConfig.h
+ */
+
+#ifndef CHIPCONFIG_H
+#define	CHIPCONFIG_H
+
+#define FCY 60000000
+void InitOscillator();
+
+#endif	/* CHIPCONFIG_H */
